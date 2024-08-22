2c3443738121bc45848b65fbdf7151a0f105ff6e
kubek <jd707@outlook.com>
2024-08-22 13:49:47 +0700

    asss

--- app.py
+++ app.py
@@ -3,24 +3,24 @@ import os
 from quote_page import show_quote_page
 from bmi_calculator import show_bmi_calculator
 
-def get_host():
-    # Próbujemy pobrać hosta z nagłówka HTTP
-    host = st.experimental_get_query_params().get('host', [''])[0] or \
-           os.environ.get('HOST', '')
-    return host.lower()
+
+
+def get_full_url():
+    # Pobieramy pełny URL z nagłówków HTTP
+    return st.get_option("server.baseUrlPath")
 
 def main():
-    host = get_host()
+    full_url = get_full_url()
     
-    if 'quotes.diduknow.org' in host:
+    if 'quotes.diduknow.org' in full_url:
         show_quote_page()
-    elif 'bmi.diduknow.org' in host:
+    elif 'bmi.diduknow.org' in full_url:
         show_bmi_calculator()
     else:
         st.write("Welcome to the main page of diduknow.org. Please use a specific subdomain.")
 
-    # Wyświetlamy aktualny host dla celów debugowania
-    st.write(f"Current host: {host}")
+    # Dla debugowania
+    st.write(f"Current URL: {full_url}")
 
 if __name__ == "__main__":
     main()
