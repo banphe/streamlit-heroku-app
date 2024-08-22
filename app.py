bd8e7384c2b9e4baa9ed527f1110bf64410aadd1
kubek <jd707@outlook.com>
2024-08-22 11:37:48 +0700

    Updated to use st.query_params instead of experimental_get_query_params

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from quote_page import show_quote_page
 from bmi_calculator import show_bmi_calculator
 
 def main():
-    host = st.query_params().get('host', [''])[0]
+    # Pobieramy wartość 'host' z parametrów URL
+    host = st.query_params.get("host", "")
     
     if 'quotes.diduknow.org' in host:
         show_quote_page()
@@ -11,6 +12,9 @@ def main():
         show_bmi_calculator()
     else:
         st.write("Welcome to the main page of diduknow.org. Please use a specific subdomain.")
+
+    # Opcjonalnie, możemy wyświetlić aktualny host dla celów debugowania
+    st.write(f"Current host: {host}")
 
 if __name__ == "__main__":
     main()
