c50ab8e02a999e3824f09acc724aa31d6adf2918
kubek <jd707@outlook.com>
2024-08-22 11:09:17 +0700

    adding dns and subdomains

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from quote_page import show_quote_page
 from bmi_calculator import show_bmi_calculator
 
 def main():
-    host = st.experimental_get_query_params().get('host', [''])[0]
+    host = st.query_params().get('host', [''])[0]
     
-    if 'quotes' in host:
+    if 'quotes.diduknow.org' in host:
         show_quote_page()
-    elif 'bmi' in host:
+    elif 'bmi.diduknow.org' in host:
         show_bmi_calculator()
     else:
-        st.write("Welcome to the main page. Please use a specific subdomain.")
+        st.write("Welcome to the main page of diduknow.org. Please use a specific subdomain.")
 
 if __name__ == "__main__":
     main()
