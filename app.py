30bc5f4dd3a674d8c5d2c3931564d65328e1caba
kubek <jd707@outlook.com>
2024-08-22 13:32:41 +0700

    pznw

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import streamlit as st
+import os
 from quote_page import show_quote_page
 from bmi_calculator import show_bmi_calculator
 
+def get_host():
+    # Próbujemy pobrać hosta z nagłówka HTTP
+    host = st.experimental_get_query_params().get('host', [''])[0] or \
+           os.environ.get('HOST', '')
+    return host.lower()
+
 def main():
-    # Pobieramy wartość 'host' z parametrów URL
-    host = st.query_params.get("host", "")
+    host = get_host()
     
     if 'quotes.diduknow.org' in host:
         show_quote_page()
@@ -13,7 +19,7 @@ def main():
     else:
         st.write("Welcome to the main page of diduknow.org. Please use a specific subdomain.")
 
-    # Opcjonalnie, możemy wyświetlić aktualny host dla celów debugowania
+    # Wyświetlamy aktualny host dla celów debugowania
     st.write(f"Current host: {host}")
 
 if __name__ == "__main__":
